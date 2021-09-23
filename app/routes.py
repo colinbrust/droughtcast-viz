29e3ecc137078777dc1bbb3c9311d00c97f1b9c1
colinbrust <colin.brust@gmail.com>
2021-09-23 17:17:38 -0600

    Got bokeh working in Flask app.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,30 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from bokeh.embed import components
+from bokeh.resources import CDN
+from bokeh.layouts import gridplot
+import datetime as dt
+from .utils.plot_map import plot_map
 import os
+import rasterio as rio
+import geopandas as gpd
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'DirtSat Admin'}
-    return render_template('index.html', title='Home', user=user)
+    raster = rio.open('/Users/colinbrust/projects/droughtcast-viz/data/template.tif')
+    r2 = rio.open('/Users/colinbrust/projects/DroughtCast/data/models/all_results/median/20070619_preds_None.tif')
+    states = gpd.read_file('/Users/colinbrust/projects/droughtcast-viz/data/states.shp')
+    date = dt.date(2015, 1, 1)
+
+    p1 = plot_map(raster, states, date, False)
+    p2 = plot_map(r2, states, date, True)
+
+    p2.x_range = p1.x_range
+    p2.y_range = p1.y_range
+    out = gridplot([[p1, p2]])
+
+    plot_script, plot_div = components(out)
+    cdn_js = CDN.js_files[0]
+    return render_template('base.html', title='egg', plot_script=plot_script, plot_div=plot_div, cdn_js=cdn_js)
---
+++ app/utils/plot_map.py
@@ -0,0 +1,73 @@
+import geopandas as gpd
+import numpy as np
+import rasterio as rio
+from bokeh.plotting import figure, show
+from xyzservices import TileProvider
+from bokeh.tile_providers import get_provider
+from bokeh.models.mappers import CategoricalColorMapper, LinearColorMapper
+from bokeh.models import HoverTool, ColorBar, GeoJSONDataSource
+
+
+
+def plot_map(raster, states, date, forecast=True):
+    arr = raster.read(1)
+    arr[arr == raster.nodata] = 0
+    arr = arr - 1
+    arr = arr.astype(np.int8)
+
+    geosource = GeoJSONDataSource(geojson=states.to_json())
+
+    google_tiles = get_provider(TileProvider(
+        name='Google Satellite',
+        url='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        attribution='Google'
+    ))
+
+    p = figure(title=f"{'Model Forecasted' if forecast else 'USDM'} Drought for {date}",
+            x_axis_label='X [m]',
+            y_axis_label='Y [m]',
+            match_aspect=True,
+            plot_width=650,
+            plot_height=450,
+            x_range=(-13877352, -7462261),
+            y_range=(2900000, 6350000))
+
+    p.add_tile(google_tiles)
+
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+
+    drought_image = p.image(
+        image=[np.flipud(arr)],
+        x=raster.bounds[0],
+        y=raster.bounds[1],
+        dw=raster.bounds[2]-raster.bounds[0],
+        dh=raster.bounds[3]-raster.bounds[1]
+    )
+
+    p.patches(
+        'xs', 'ys', source=geosource, 
+        line_color='black',
+        line_width = 0.5,
+        line_alpha=1,
+        fill_alpha=0
+    )
+
+    drought_image.glyph.color_mapper = LinearColorMapper(
+        low=-1, high=4,
+        palette=['rgba(0, 0, 0, 0)', '#FFFF00', '#FCD37F', '#FFAA00', '#E60000', '#730000']
+    )
+
+    legend_mapper = CategoricalColorMapper(
+        factors=['No Drought', 'D0', 'D1', 'D2', 'D3', 'D4'],
+        palette=['rgba(0, 0, 0, 0)', '#FFFF00', '#FCD37F', '#FFAA00', '#E60000', '#730000'] 
+    )
+
+    color_bar = ColorBar(color_mapper=legend_mapper, label_standoff=12)
+
+    drought_hover = HoverTool(tooltips=[('Category', 'D@image{0}')],
+                        renderers=[drought_image])
+
+    p.add_tools(drought_hover)
+    p.add_layout(color_bar, 'above')
+    return p
